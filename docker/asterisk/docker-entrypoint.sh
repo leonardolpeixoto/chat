7ff163df57b0d7b03760b32afcbf798b2b34cb37
leonardo <desenvolvimento14o@fortics.com.br>
2019-02-08 13:15:37 -0300

    [docker] asterisk config docker

---
+++ docker/asterisk/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+ASTERISK_USER=${ASTERISK_USER:-asterisk}
+ASTERISK_GROUP=${ASTERISK_GROUP:-${ASTERISK_USER}}
+
+if [ "$1" = "" ]; then
+  COMMAND="/usr/sbin/asterisk -T -W -U ${ASTERISK_USER} -p -vvvdddf"
+else
+  COMMAND="$@"
+fi
+
+if [ "${ASTERISK_UID}" != "" ] && [ "${ASTERISK_GID}" != "" ]; then
+  deluser asterisk && \
+  addgroup -g ${ASTERISK_GID} ${ASTERISK_GROUP} && \
+  adduser -D -H -u ${ASTERISK_UID} -G ${ASTERISK_GROUP} ${ASTERISK_USER} \
+  || exit
+fi
+
+chown -R ${ASTERISK_USER}: /var/log/asterisk \
+                           /var/lib/asterisk \
+                           /var/run/asterisk \
+                           /var/spool/asterisk; \
+exec ${COMMAND}
